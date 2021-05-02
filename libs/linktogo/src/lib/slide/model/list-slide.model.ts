620a8a1e3ad4a62b3063c9325b41479d57d9d4b0
linktogo <dev@linktogo.fr>
2021-05-02 22:09:38 +0200

    add slide

---
+++ libs/linktogo/src/lib/slide/model/list-slide.model.ts
@@ -0,0 +1,6 @@
+export interface ListSlide {
+    image?: string;
+    title?: string;
+    link?: string;
+    alt?: string;
+}
--- libs/linktogo/src/lib/slide/slide.component.ts
+++ libs/linktogo/src/lib/slide/slide.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ListSlide } from './model/list-slide.model';
 
 @Component({
-  selector: 'linktogo-ui-slide',
-  templateUrl: './slide.component.html',
-  styleUrls: ['./slide.component.scss']
+	selector: 'app-slide',
+	templateUrl: './slide.component.html',
+	styleUrls: ['./slide.component.css']
 })
 export class SlideComponent implements OnInit {
+	private max_size = 6;
+	private current = 0;
 
-  constructor() { }
+	@Input()
+	public list: ListSlide[];
 
-  ngOnInit(): void {
-  }
+	public listDisplay: ListSlide[];
 
+	ngOnInit(): void {
+		this.loadImages();
+	}
+
+	previous(): void {
+		this.current = this.current - 1;
+		this.loadImages();
+	}
+
+	next(): void {
+		this.current = this.current+ 1;
+		this.loadImages();
+	}
+
+	private loadImages() {
+		this.listDisplay = this.list.slice(this.current, this.max_size+this.current);
+	}
 }
